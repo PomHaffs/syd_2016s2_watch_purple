207c04e628098beb2e8d6eb8cf3189803911c9b1
howardmann <howardmann27@gmail.com>
2016-09-27 18:21:34 +1000

    capitalized

--- client/spec/pages/askEmotionPage.spec.js
+++ client/spec/pages/askEmotionPage.spec.js
@@ -6,7 +6,7 @@ var askEmotionPage = require('../../src/js/pages/askEmotionPage'),
 describe('The ask emotion page', function() {
 
     beforeEach(function() {
-        page = new askEmotionPage();
+        page = new AskEmotionPage();
       });
 
     describe('rendering', function() {
