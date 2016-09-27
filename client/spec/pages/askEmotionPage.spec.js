b048d184f8d560b6ab1a4b9f28f3c4fd98e74cbd
howardmann <howardmann27@gmail.com>
2016-09-27 18:22:47 +1000

    capitalize

--- client/spec/pages/askEmotionPage.spec.js
+++ client/spec/pages/askEmotionPage.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var askEmotionPage = require('../../src/js/pages/askEmotionPage'),
+var AskEmotionPage = require('../../src/js/pages/askEmotionPage'),
     page;
 
 describe('The ask emotion page', function() {
