7af17c981e7835232d195d44129c35f2e3db052b
kgkma <kgkma3@gmail.com>
2016-10-04 21:11:02 +1100

    Kevin/Laura: WIP - Added yoga poses data and tests

--- client/spec/storage/storage.spec.js
+++ client/spec/storage/storage.spec.js
@@ -5,7 +5,13 @@ var storage = require('../../src/storage');
 describe('storage', function() {
   describe('compliments', function() {
     it('should load the compliments data', function() {
-      // expect(storage.complimentsData.length).toEqual(3);
+       expect(storage.complimentsData.length).toEqual(9);
+    });
+  });
+
+  describe('poses', function() {
+    it('should load the yoga poses data', function() {
+       expect(storage.posesData.length).toEqual(3);
     });
   });
 });
--- client/src/storage/index.js
+++ client/src/storage/index.js
@@ -2,12 +2,15 @@
 
 var eventsData = require("json!./events.json");
 var complimentsData = require("json!./compliments.json");
+var posesData = require("json!./poses.json");
 
 function Storage() {
   this.eventsData = new Backbone.Collection();
   this.eventsData.add(eventsData.events);
   this.complimentsData = new Backbone.Collection();
   this.complimentsData.add(complimentsData.compliments);
+  this.posesData = new Backbone.Collection();
+  this.posesData.add(posesData.poses);
 }
 
 module.exports = new Storage();
