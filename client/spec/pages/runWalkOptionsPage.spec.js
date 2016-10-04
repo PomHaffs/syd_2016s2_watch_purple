3dfa9a658bbcf411a37f603df59e88f5f969afa7
lucy <lucy>
2016-10-04 20:47:04 +1100

    l

--- client/spec/pages/runWalkOptionsPage.spec.js
+++ client/spec/pages/runWalkOptionsPage.spec.js
@@ -37,4 +37,25 @@ describe('The RUN/WALK Options Page', function() {
       });
     });
   });
+    describe('button event handlers', function() {
+	
+	describe('face', function() {
+	  it('should take the user back to the home page', function() {
+	    spyOn(window.App, 'navigate');
+	    page.configureButtons();
+	    eventHub.trigger('face');
+	    expect(window.App.navigate).toHaveBeenCalledWith('home');
+	  });
+	});
+    });
 });
+
+
+
+
+
+
+
+
+
+
